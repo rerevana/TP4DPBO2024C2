b81d2212e891e92b316ec837c2e7d62a1fa5e363
rerevana <revanafalihas@gmail.com>
2024-05-07 17:49:09 +0700

    final update

--- conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-  class Conf{
-    static public $db_host = "localhost";
-    static public $db_user = "root";
-    static public $db_pass = "";
-    static public $db_name = "tp_mvc";
-  }
-?>
---
+++ tp_mvc.sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 07, 2024 at 12:39 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tp_mvc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `anggota`
+--
+
+CREATE TABLE `anggota` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `telp` varchar(20) NOT NULL,
+  `join_date` date NOT NULL,
+  `fandom` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `anggota`
+--
+
+INSERT INTO `anggota` (`id`, `nama`, `email`, `telp`, `join_date`, `fandom`) VALUES
+(2, 'Rere', 'rere@gmail.com', '09828728827', '2024-05-09', 1),
+(4, 'Veronika', 'vero@gmail.com', '089598768352', '2022-06-22', 8),
+(5, 'Lala', 'lala@gmail.com', '082134623876', '2019-01-07', 1),
+(7, 'Angelina', 'angel@gmail.com', '087654215674', '2024-02-06', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fandom`
+--
+
+CREATE TABLE `fandom` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `fandom`
+--
+
+INSERT INTO `fandom` (`id`, `nama`) VALUES
+(1, 'Army'),
+(2, 'Carat'),
+(8, 'Buddy'),
+(9, 'ReVeluv'),
+(10, 'EXO-L'),
+(11, 'MyDay');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `anggota`
+--
+ALTER TABLE `anggota`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `relasi` (`fandom`);
+
+--
+-- Indexes for table `fandom`
+--
+ALTER TABLE `fandom`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `anggota`
+--
+ALTER TABLE `anggota`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `fandom`
+--
+ALTER TABLE `fandom`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `anggota`
+--
+ALTER TABLE `anggota`
+  ADD CONSTRAINT `relasi` FOREIGN KEY (`fandom`) REFERENCES `fandom` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
